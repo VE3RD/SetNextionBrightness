57cc94128d5fafbfb9a6982f7a411deef1cb3740
phil <ve3rd@rac.ca>
2021-10-30 22:14:55 -0400

    update sunwait install

--- install.sh
+++ install.sh
@@ -4,12 +4,18 @@ if [ ! -d /home/pi-star/sunwait ]; then
 	cd /home/pi-star/
 	sudo mount -o remount,rw /
 
-   	sudo apt-get install sunwait
+#   	sudo apt-get install sunwait
+	
+        sudo git clone https://github.com/risacher/sunwait.git
 	cd sunwait
+	make clean
 	make
+	echo "Ignore the warnings"
 	sudo cp sunwait /usr/local/bin/
 else
 	echo "sunwait previously installed"
+	echo "if you have a problem with sunwait, delete /home/pi-star/sunwait"
+	echo " and re-run this install script"
 fi
 sleep 3
 cd /home/pi-star/SetNextionBrightness
